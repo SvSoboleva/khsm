15f78e06cc0e0632b5ca62268d13158bf3cedc48
Lana <sv.soboleva@list.ru>
2017-06-14 20:54:52 +0300

    integral tests new version

--- spec/features/user_gives_wrong_answer_spec.rb
+++ spec/features/user_gives_wrong_answer_spec.rb
@@ -2,51 +2,30 @@
 require 'rails_helper'
 
 # Начинаем описывать функционал, связанный с созданием игры
-RSpec.feature 'USER creates a game', type: :feature do
-  # Чтобы пользователь мог начать игру, нам надо
-  # создать пользователя
-  let(:user) { FactoryGirl.create(:user, name: 'Мария') }
+RSpec.feature 'USER gives wrong answer', type: :feature do
 
-  # и создать 15 вопросов с разными уровнями сложности
-  # Обратите внимание, что текст вопроса и вариантов ответа нам
-  # здесь важен, так как именно их мы потом будем проверяеть
-  let!(:questions) do
-    (0..14).to_a.map do |i|
-      FactoryGirl.create(
-        :question, level: i,
-        text: "Когда была куликовская битва номер #{i}?",
-        answer1: '1380', answer2: '1381', answer3: '1382', answer4: '1383'
-      )
-    end
-  end
+  let(:game_w_questions) { FactoryGirl.create(:game_with_questions) }
 
   # Перед началом любого сценария нам надо авторизовать пользователя
   before(:each) do
-    login_as user
+    login_as game_w_questions.user
   end
 
   # Сценарий успешного создания игры
   scenario 'successfully' do
-    # Заходим на главную
-    visit '/'
-
-    # Кликаем по ссылке "Новая игра"
-    click_link 'Новая игра'
-
-    # Ожидаем, что попадем на нужный url
-    expect(page).to have_current_path '/games/1'
+    #game = game_w_questions.game
+    visit "/games/1"
 
     # Ожидаем, что трижды правильно ответили на вопрос
-    (0..2).to_a.map do |i|
-      expect(page).to have_content "Когда была куликовская битва номер #{i}?"
-      click_link '1380'
+    (1..3).to_a.map do |i|
+      expect(page).to have_content "В каком году была космичесая одиссея #{i}?"
+      click_link 'D'
     end
 
     # неправильный ответ
-    click_link '1381'
+    click_link 'A'
 
     # Ожидаем, что на экране профиль игрока
-    expect(page).to have_content 'Мария'
     expect(page).to have_content 'проигрыш'
     expect(page).to have_content '0 ₽'
   end
--- spec/features/user_views_user_rating_spec.rb
+++ spec/features/user_views_user_rating_spec.rb
@@ -2,26 +2,13 @@
 require 'rails_helper'
 
 # Начинаем описывать функционал, связанный с созданием игры
-RSpec.feature 'USER looks a rating', type: :feature do
+RSpec.feature 'USER views a rating', type: :feature do
   # Чтобы пользователь мог начать игру, нам надо
   # создать пользователя
   let(:users) { [
     FactoryGirl.create(:user, name: 'Мария', balance: 3000),
     FactoryGirl.create(:user, name: 'Артем', balance: 5000)
   ] }
-
-  # и создать 15 вопросов с разными уровнями сложности
-  # Обратите внимание, что текст вопроса и вариантов ответа нам
-  # здесь важен, так как именно их мы потом будем проверяеть
-  let!(:questions) do
-    (0..14).to_a.map do |i|
-      FactoryGirl.create(
-        :question, level: i,
-        text: "Когда была куликовская битва номер #{i}?",
-        answer1: '1380', answer2: '1381', answer3: '1382', answer4: '1383'
-      )
-    end
-  end
 
   # Перед началом любого сценария нам надо авторизовать пользователя
   before(:each) do
